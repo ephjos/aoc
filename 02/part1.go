4aba6664e6752e1220138afdd31da806caa6fd5f
Joseph Hines <jthines3@gmail.com>
2019-12-02 00:33:49 -0500

    .

--- 02/part1.go
+++ 02/part1.go
@@ -6,13 +6,37 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	input := bufio.NewScanner(os.Stdin)
+	input.Scan()
+	temp := strings.Split(input.Text(), ",")
+	var data []int
 
-	for input.Scan() {
-		line := input.Text()
-		fmt.Println(line)
+	for _, v := range temp {
+		t, _ := strconv.Atoi(v)
+		data = append(data, t)
 	}
+
+	for i := 0; i < len(data); i += 4 {
+		opcode := data[i]
+		if opcode == 99 {
+			break
+		}
+
+		i1 := data[i+1]
+		i2 := data[i+2]
+		d := data[i+3]
+
+		if opcode == 1 {
+			data[d] = data[i1] + data[i2]
+		} else if opcode == 2 {
+			data[d] = data[i1] * data[i2]
+		}
+	}
+
+	fmt.Println(data)
 }
--- 02/part2.go
+++ 02/part2.go
@@ -1,4 +1,4 @@
-// Part 2
+// Part 1
 
 package main
 
@@ -6,13 +6,54 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
+
+func runIntCode(memory []int, noun, verb int) int {
+	memory[1] = noun
+	memory[2] = verb
+
+	for i := 0; i < len(memory); i += 4 {
+		opcode := memory[i]
+		if opcode == 99 {
+			break
+		}
+
+		i1 := memory[i+1]
+		i2 := memory[i+2]
+		d := memory[i+3]
+
+		if opcode == 1 {
+			memory[d] = memory[i1] + memory[i2]
+		} else if opcode == 2 {
+			memory[d] = memory[i1] * memory[i2]
+		}
+	}
+
+	return memory[0]
+}
 
 func main() {
 	input := bufio.NewScanner(os.Stdin)
+	input.Scan()
+	temp := strings.Split(input.Text(), ",")
+	var data []int
 
-	for input.Scan() {
-		line := input.Text()
-		fmt.Println(line)
+	for _, v := range temp {
+		t, _ := strconv.Atoi(v)
+		data = append(data, t)
+	}
+
+	for i := 0; i < 99; i++ {
+		for j := 0; j < 99; j++ {
+			var dataCopy = make([]int, len(data))
+			copy(dataCopy, data)
+			output := runIntCode(dataCopy, i, j)
+			if output == 19690720 {
+				fmt.Println(100*i + j)
+				return
+			}
+		}
 	}
 }
