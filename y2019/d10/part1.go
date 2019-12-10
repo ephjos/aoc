2bc9dad2098d6f9249dcec5037a0f74a00bdf903
Joseph Hines <jthines3@gmail.com>
2019-12-10 17:33:14 -0500

    .

--- y2019/d10/part1.go
+++ y2019/d10/part1.go
@@ -8,11 +8,31 @@ import (
 	"os"
 )
 
+type Point struct {
+	X int // Distance from left
+	Y int // Distance from top
+}
+
+func getAsteroidPoints(scanner *bufio.Scanner) []Point {
+	points := make([]Point, 0)
+
+	j := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		for i, v := range line {
+			if string(v) == "#" {
+				points = append(points, Point{i, j})
+			}
+		}
+	}
+
+	j += 1
+
+	return points
+}
+
 func main() {
 	input := bufio.NewScanner(os.Stdin)
-
-	for input.Scan() {
-		line := input.Text()
-		fmt.Println(line)
-	}
+	points := getAsteroidPoints(input)
+	fmt.Println(points)
 }
