b86c54f5ce8ff7c8044e014a86deabc50d0849d2
Joseph Hines <jthines3@gmail.com>
2019-12-13 14:35:11 -0500

    .

--- y2019/d10/part1.go
+++ y2019/d10/part1.go
@@ -4,120 +4,90 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"math"
 	"os"
-	"time"
-
-	"github.com/faiface/pixel"
-	"github.com/faiface/pixel/imdraw"
-	"github.com/faiface/pixel/pixelgl"
-	"golang.org/x/image/colornames"
 )
 
-type Point struct {
-	X float64 // Distance from left
-	Y float64 // Distance from top
+type Slope struct {
+	rise, run float64
 }
 
-func getAsteroidPoints(scanner *bufio.Scanner) (map[Point]int, int, int) {
-	points := make(map[Point]int, 0)
-	h := 0
-	count := 0
+type Point struct {
+	x, y int
+}
 
-	j := 0
-	for scanner.Scan() {
-		line := scanner.Text()
-		for i, v := range line {
-			if string(v) == "#" {
-				points[Point{float64(i), float64(j)}] = 0
-			}
-			count += 1
-		}
-		j += 1
+func gcd(a, b float64) float64 {
+	for b != 0 {
+		t := b
+		b = math.Mod(a, b)
+		a = t
 	}
 
-	h = j
-	w := count / j
-
-	return points, w, h
+	return math.Abs(a)
 }
 
-func visualize(points *map[Point]int, w, h int) {
-	pixelgl.Run(func() {
-		cfg := pixelgl.WindowConfig{
-			Title:  "SIF Visualizer",
-			Bounds: pixel.R(0, 0, 768, 768),
+func getAsteroidPoints(scanner *bufio.Scanner) []Point {
+	points := make([]Point, 0)
+
+	i := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		for j, v := range line {
+			if string(v) == "#" {
+				points = append(
+					points,
+					Point{i, j},
+				)
+			}
 		}
-		win, err := pixelgl.NewWindow(cfg)
-		if err != nil {
-			panic("Cannot load OpenGL, your version may be too old...")
-		}
+		i += 1
+	}
 
-		offset := 20.
-		bounds := win.Bounds()
-		height := bounds.H() - (2 * offset)
-		width := bounds.W() - offset
+	return points
+}
 
-		wStep := width / float64(w)
-		hStep := height / float64(h)
-		offsetVec := pixel.V(offset, offset)
+func getMostInView(points []Point) {
+	slopes := make(map[Point]map[Slope]int, 0)
 
-		imd := imdraw.New(nil)
-		lines := imdraw.New(nil)
+	for _, center := range points {
+		for _, pt := range points {
+			if pt != center {
+				deltaY := float64(pt.y - center.y)
+				deltaX := float64(pt.x - center.x)
+				gcd := gcd(deltaY, deltaX)
 
-		vecs := make([]pixel.Vec, 0)
-
-		for point := range *points {
-			vec := pixel.V(point.X*wStep, height-(point.Y*hStep)).Add(offsetVec)
-			vecs = append(vecs, vec)
-			imd.Push(vec)
-			imd.Circle(5, 1)
-		}
-
-		i := 0
-		for !win.Closed() {
-			win.Clear(colornames.Black)
-
-			imd.Draw(win)
-
-			if i < len(vecs) {
-				lines.Clear()
-
-				scale := 200.
-				res := 30.
-				base := pixel.V(0, 0)
-
-				for j := 0.; j < res; j++ {
-					rads := j * (360. / res) * (math.Pi / 180.)
-
-					xAng := math.Cos(rads)
-					yAng := math.Sin(rads)
-					x := scale * xAng
-					y := scale * yAng
-
-					base = pixel.V(x, y).Add(vecs[i])
-
-					lines.Color = pixel.RGB(1, 1, 1)
-					lines.Push(vecs[i], base)
-					lines.Line(1)
+				if gcd != 0 {
+					deltaY /= gcd
+					deltaX /= gcd
 				}
 
-				lines.Draw(win)
+				slope := Slope{deltaY, deltaX}
 
-				i += 1
-				time.Sleep(time.Second / 2.)
+				if slopes[center] != nil {
+					slopes[center][slope] += 1
+				} else {
+					slopes[center] = make(map[Slope]int, 0)
+					slopes[center][slope] = 1
+				}
 			}
-
-			win.Update()
-
 		}
-	})
+	}
 
+	max := 0
+	for _, s := range slopes {
+		count := len(s)
+		if count > max {
+			max = count
+		}
+	}
+
+	fmt.Println(max)
 }
 
 func main() {
 	input := bufio.NewScanner(os.Stdin)
-	points, w, h := getAsteroidPoints(input)
+	points := getAsteroidPoints(input)
 
-	visualize(&points, w, h)
+	getMostInView(points)
 }
