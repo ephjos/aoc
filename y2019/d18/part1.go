a2d8cf1ddc2102cc074798eda949e03dcf17fd77
Joseph Hines <jthines3@gmail.com>
2019-12-23 12:43:20 -0500

    .

--- y2019/d18/part1.go
+++ y2019/d18/part1.go
@@ -5,72 +5,18 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 )
 
 var maze = make(map[Point]string, 0)
-var start = Point{0, 0}
+var startPoint = Point{0, 0}
 var ALL_KEYS = make([]string, 0)
 
 type Point struct {
 	x, y int
-}
-
-type KeyInfo struct {
-	point Point
-	name  string
-}
-
-type KeySet struct {
-	data []KeyInfo
-}
-
-func (k *KeySet) Contains(ki KeyInfo) bool {
-	for _, v := range k.data {
-		if v == ki {
-			return true
-		}
-	}
-
-	return false
-}
-
-func (k *KeySet) ContainsName(s string) bool {
-	for _, v := range k.data {
-		if v.name == s {
-			return true
-		}
-	}
-
-	return false
-}
-
-func (k *KeySet) Add(ki KeyInfo) {
-	if !k.Contains(ki) {
-		k.data = append(k.data, ki)
-	}
-}
-
-func (k *KeySet) Combine(ki *KeySet) *KeySet {
-	out := &KeySet{[]KeyInfo{}}
-
-	for _, v := range k.data {
-		out.Add(v)
-	}
-	for _, v := range ki.data {
-		out.Add(v)
-	}
-
-	return out
-}
-
-func (k *KeySet) Copy() *KeySet {
-	out := make([]KeyInfo, len(k.data))
-	copy(out, k.data)
-
-	return &KeySet{out}
 }
 
 const (
@@ -78,6 +24,103 @@ const (
 	WALL   = "#"
 	PLAYER = "@"
 )
+
+type XYKeys struct {
+	point Point
+	keys  []string
+}
+
+func NewXYKeys(point Point) *XYKeys {
+	return &XYKeys{point, []string{}}
+}
+
+func (x *XYKeys) ContainsKey(k string) bool {
+	for _, v := range x.keys {
+		if v == k {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (x *XYKeys) AddKey(k string) {
+	if !x.ContainsKey(k) {
+		x.keys = append(x.keys, k)
+	}
+
+	sort.Strings(x.keys)
+
+}
+
+func (x *XYKeys) Copy() *XYKeys {
+	out := NewXYKeys(x.point)
+	out.keys = make([]string, len(x.keys))
+	copy(out.keys, x.keys)
+	return out
+}
+
+func (x *XYKeys) Hash() string {
+	out := ""
+	out += strconv.Itoa(x.point.x) + ","
+	out += strconv.Itoa(x.point.y) + ","
+
+	for _, v := range x.keys {
+		out += v + ","
+	}
+
+	return out
+}
+
+type XYKeysQueue struct {
+	items []XYKeys
+}
+
+func NewXYKeysQueue() *XYKeysQueue {
+	return &XYKeysQueue{[]XYKeys{}}
+}
+
+func (xq *XYKeysQueue) Enqueue(x *XYKeys) {
+	xq.items = append(xq.items, *x)
+}
+
+func (xq *XYKeysQueue) EnqueueS(x *XYKeys, dist map[string]int) {
+	xq.items = append(xq.items, *x)
+
+	sort.Slice(xq.items, func(i, j int) bool {
+		d1 := dist[xq.items[i].Hash()]
+		d2 := dist[xq.items[j].Hash()]
+		return d1 < d2
+	})
+}
+
+func (xq *XYKeysQueue) Dequeue() *XYKeys {
+	item := xq.items[0]
+	xq.items = xq.items[1:]
+	return &item
+}
+
+func (xq *XYKeysQueue) Front() XYKeys {
+	return xq.items[0]
+}
+
+func (xq *XYKeysQueue) IsEmpty() bool {
+	return len(xq.items) == 0
+}
+
+func (xq *XYKeysQueue) Size() int {
+	return len(xq.items)
+}
+
+func StringContains(arr []string, s string) bool {
+	for _, v := range arr {
+		if v == s {
+			return true
+		}
+	}
+
+	return false
+}
 
 func parseMap(strings []string) map[Point]string {
 	m := make(map[Point]string, 0)
@@ -88,7 +131,8 @@ func parseMap(strings []string) map[Point]string {
 		for _, c := range str {
 
 			if string(c) == PLAYER {
-				start = currPoint
+				startPoint = currPoint
+				m[currPoint] = OPEN
 			} else {
 				m[currPoint] = string(c)
 				if string(c) != WALL && string(c) != OPEN &&
@@ -106,8 +150,9 @@ func parseMap(strings []string) map[Point]string {
 	return m
 }
 
-func print(m map[Point]string) {
+func print(m map[Point]string, current *XYKeys) {
 	size := 81
+	//size := 20
 	out := "\x1b[2;0H"
 
 	for i := 0; i < size; i++ {
@@ -117,8 +162,12 @@ func print(m map[Point]string) {
 			clr := 40
 			ch := m[p]
 
-			if p == start {
+			if p == current.point {
 				ch = PLAYER
+			}
+
+			if current.ContainsKey(ch) {
+				ch = OPEN
 			}
 
 			switch ch {
@@ -131,7 +180,7 @@ func print(m map[Point]string) {
 				ch = " "
 			default:
 				if ch != "" {
-					clr = 103
+					clr = 104
 				}
 			}
 			ch = "" + ch + " "
@@ -144,78 +193,104 @@ func print(m map[Point]string) {
 	fmt.Println(out)
 }
 
-func score(point Point, ks *KeySet, visited map[Point]bool) int {
-	if len(ALL_KEYS) == len(ks.data) {
-		return 0
-	}
-
-	if visited[point] {
-		return 0
-	}
-
-	visited[point] = true
-
-	scores := make([]int, 0)
-
-	for _, k := range reachable(point, ks, make(map[Point]bool)).data {
-		nKs := ks.Copy()
-		nKs.Add(k)
-		s := score(k.point, nKs, visited) + 1 // + distance from point to k.point
-		scores = append(scores, s)
-	}
-
-	min := math.MaxInt32
-
-	for _, s := range scores {
-		if s < min {
-			min = s
-		}
-	}
-
-	fmt.Println(point)
-	fmt.Println(ks)
-	fmt.Println(visited)
-	fmt.Println(scores)
-	fmt.Println()
-
-	return min
-}
-
-func reachable(point Point, ks *KeySet, visited map[Point]bool) *KeySet {
-	if visited[point] {
-		return ks
-	}
-
-	visited[point] = true
-	nbs := []Point{
+func GetNeighbors(point Point) []Point {
+	return []Point{
 		Point{point.x - 1, point.y},
 		Point{point.x + 1, point.y},
 		Point{point.x, point.y - 1},
 		Point{point.x, point.y + 1},
 	}
+}
 
-	out := ks.Copy()
+func DFS() (Point, int) {
 
-	for _, nb := range nbs {
-		v := maze[nb]
+	// Initialize dist map, track distance to each
+	// XYKeys object
+	dist := make(map[string]int, 0)
 
-		switch v {
-		case WALL:
-		case OPEN:
-			out = out.Combine(reachable(nb, ks, visited))
-		case strings.ToLower(v):
-			if v != "" && !ks.ContainsName(v) {
-				ks.Add(KeyInfo{nb, v})
-				out = out.Combine(reachable(nb, ks, visited))
-			}
-		case strings.ToUpper(v):
-			if ks.ContainsName(strings.ToLower(v)) {
-				out = out.Combine(reachable(nb, ks, visited))
-			}
+	// Create initial start XYKeys point
+	start := NewXYKeys(startPoint)
+
+	// Set start distance to 0
+	dist[start.Hash()] = 0
+
+	// Create queue
+	q := NewXYKeysQueue()
+
+	// Add start point to Queue
+	q.Enqueue(start)
+
+	// While there are still items in the Queue
+	for !q.IsEmpty() {
+		// Get front of queue
+		u := q.Dequeue()
+
+		// Get the current keys
+		ukeys := u.keys
+
+		// If we have all of the keys,
+		// return the current point and distance to it
+		if len(ukeys) == len(ALL_KEYS) {
+			return u.point, dist[u.Hash()]
 		}
+
+		// Get known distance for current XYKeys
+		udist := dist[u.Hash()]
+
+		//print(maze, u)
+		fmt.Println("\x1b[2;0H")
+		fmt.Println("All keys:", ALL_KEYS)
+		fmt.Println("Current point + keys:", u)
+		fmt.Println("Current distance:", udist)
+		fmt.Println("\x1b[0m\n")
+		//time.Sleep(time.Second / 100.)
+
+		// For each neighbor (U,D,L,R)
+		for _, nb := range GetNeighbors(u.point) {
+			// Get value at point in map
+			char := maze[nb]
+
+			// Create XYKeys at nb point
+			v := u.Copy()
+			v.point = nb
+
+			// If nb is a wall, do nothing
+			if char == WALL {
+				continue
+			}
+
+			// If nb is on a lowercase letter (key),
+			// try to add key to v
+			if char == strings.ToLower(char) &&
+				char != OPEN {
+				v.AddKey(char)
+			}
+
+			// If nb is on an uppercase letter (door)
+			if char == strings.ToUpper(char) &&
+				char != OPEN {
+				// If we don't have the key,
+				// do nothing
+				if !v.ContainsKey(strings.ToLower(char)) {
+					continue
+				}
+			}
+
+			// If we have a distance record already,
+			// do nothing
+			if dist[v.Hash()] != 0 {
+				continue
+			}
+
+			// Otherwise, update distance and add to queue
+			dist[v.Hash()] = udist + 1
+			//q.EnqueueS(v, dist)
+			q.Enqueue(v)
+		}
+
 	}
 
-	return out
+	return startPoint, -1
 }
 
 func main() {
@@ -228,7 +303,8 @@ func main() {
 	}
 
 	maze = parseMap(strings)
-	ks := &KeySet{[]KeyInfo{}}
 
-	fmt.Println(score(start, ks, make(map[Point]bool)))
+	fmt.Println()
+	fmt.Println(DFS())
+
 }
--- y2019/d18/part2.go
+++ y2019/d18/part2.go
@@ -6,13 +6,305 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
+
+var maze = make(map[Point]string, 0)
+var startPoint = Point{0, 0}
+var ALL_KEYS = make([]string, 0)
+
+type Point struct {
+	x, y int
+}
+
+const (
+	OPEN   = "."
+	WALL   = "#"
+	PLAYER = "@"
+)
+
+type XYKeys struct {
+	point Point
+	keys  []string
+}
+
+func NewXYKeys(point Point) *XYKeys {
+	return &XYKeys{point, []string{}}
+}
+
+func (x *XYKeys) ContainsKey(k string) bool {
+	for _, v := range x.keys {
+		if v == k {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (x *XYKeys) AddKey(k string) {
+	if !x.ContainsKey(k) {
+		x.keys = append(x.keys, k)
+	}
+
+	sort.Strings(x.keys)
+
+}
+
+func (x *XYKeys) Copy() *XYKeys {
+	out := NewXYKeys(x.point)
+	out.keys = make([]string, len(x.keys))
+	copy(out.keys, x.keys)
+	return out
+}
+
+func (x *XYKeys) Hash() string {
+	out := ""
+	out += strconv.Itoa(x.point.x) + ","
+	out += strconv.Itoa(x.point.y) + ","
+
+	for _, v := range x.keys {
+		out += v + ","
+	}
+
+	return out
+}
+
+type XYKeysQueue struct {
+	items []XYKeys
+}
+
+func NewXYKeysQueue() *XYKeysQueue {
+	return &XYKeysQueue{[]XYKeys{}}
+}
+
+func (xq *XYKeysQueue) Enqueue(x *XYKeys) {
+	xq.items = append(xq.items, *x)
+}
+
+func (xq *XYKeysQueue) EnqueueS(x *XYKeys, dist map[string]int) {
+	xq.items = append(xq.items, *x)
+
+	sort.Slice(xq.items, func(i, j int) bool {
+		d1 := dist[xq.items[i].Hash()]
+		d2 := dist[xq.items[j].Hash()]
+		return d1 < d2
+	})
+}
+
+func (xq *XYKeysQueue) Dequeue() *XYKeys {
+	item := xq.items[0]
+	xq.items = xq.items[1:]
+	return &item
+}
+
+func (xq *XYKeysQueue) Front() XYKeys {
+	return xq.items[0]
+}
+
+func (xq *XYKeysQueue) IsEmpty() bool {
+	return len(xq.items) == 0
+}
+
+func (xq *XYKeysQueue) Size() int {
+	return len(xq.items)
+}
+
+func StringContains(arr []string, s string) bool {
+	for _, v := range arr {
+		if v == s {
+			return true
+		}
+	}
+
+	return false
+}
+
+func parseMap(strings []string) map[Point]string {
+	m := make(map[Point]string, 0)
+
+	currPoint := Point{0, 0}
+
+	for _, str := range strings {
+		for _, c := range str {
+
+			if string(c) == PLAYER {
+				startPoint = currPoint
+				m[currPoint] = OPEN
+			} else {
+				m[currPoint] = string(c)
+				if string(c) != WALL && string(c) != OPEN &&
+					(int(c) > 96 && int(c) < 123) {
+					ALL_KEYS = append(ALL_KEYS, string(c))
+				}
+			}
+
+			currPoint.x++
+		}
+		currPoint.x = 0
+		currPoint.y++
+	}
+
+	return m
+}
+
+func print(m map[Point]string, current *XYKeys) {
+	size := 81
+	//size := 20
+	out := "\x1b[2;0H"
+
+	for i := 0; i < size; i++ {
+		for j := 0; j < size; j++ {
+			p := Point{j, i}
+
+			clr := 40
+			ch := m[p]
+
+			if p == current.point {
+				ch = PLAYER
+			}
+
+			if current.ContainsKey(ch) {
+				ch = OPEN
+			}
+
+			switch ch {
+			case WALL:
+				clr = 47
+				ch = " "
+			case PLAYER:
+				clr = 106
+			case OPEN:
+				ch = " "
+			default:
+				if ch != "" {
+					clr = 104
+				}
+			}
+			ch = "" + ch + " "
+			out += fmt.Sprintf("\x1b[%dm%s", clr, string(ch))
+
+		}
+		out += fmt.Sprint("\x1b[0m\n")
+	}
+
+	fmt.Println(out)
+}
+
+func GetNeighbors(point Point) []Point {
+	return []Point{
+		Point{point.x - 1, point.y},
+		Point{point.x + 1, point.y},
+		Point{point.x, point.y - 1},
+		Point{point.x, point.y + 1},
+	}
+}
+
+func DFS() (Point, int) {
+
+	// Initialize dist map, track distance to each
+	// XYKeys object
+	dist := make(map[string]int, 0)
+
+	// Create initial start XYKeys point
+	start := NewXYKeys(startPoint)
+
+	// Set start distance to 0
+	dist[start.Hash()] = 0
+
+	// Create queue
+	q := NewXYKeysQueue()
+
+	// Add start point to Queue
+	q.Enqueue(start)
+
+	// While there are still items in the Queue
+	for !q.IsEmpty() {
+		// Get front of queue
+		u := q.Dequeue()
+
+		// Get the current keys
+		ukeys := u.keys
+
+		// If we have all of the keys,
+		// return the current point and distance to it
+		if len(ukeys) == len(ALL_KEYS) {
+			return u.point, dist[u.Hash()]
+		}
+
+		// Get known distance for current XYKeys
+		udist := dist[u.Hash()]
+
+		//print(maze, u)
+		fmt.Println("\x1b[2;0H")
+		fmt.Println("All keys:", ALL_KEYS)
+		fmt.Println("Current point + keys:", u)
+		fmt.Println("Current distance:", udist)
+		fmt.Println("\x1b[0m\n")
+		//time.Sleep(time.Second / 100.)
+
+		// For each neighbor (U,D,L,R)
+		for _, nb := range GetNeighbors(u.point) {
+			// Get value at point in map
+			char := maze[nb]
+
+			// Create XYKeys at nb point
+			v := u.Copy()
+			v.point = nb
+
+			// If nb is a wall, do nothing
+			if char == WALL {
+				continue
+			}
+
+			// If nb is on a lowercase letter (key),
+			// try to add key to v
+			if char == strings.ToLower(char) &&
+				char != OPEN {
+				v.AddKey(char)
+			}
+
+			// If nb is on an uppercase letter (door)
+			if char == strings.ToUpper(char) &&
+				char != OPEN {
+				// If we don't have the key,
+				// do nothing
+				if !v.ContainsKey(strings.ToLower(char)) {
+					continue
+				}
+			}
+
+			// If we have a distance record already,
+			// do nothing
+			if dist[v.Hash()] != 0 {
+				continue
+			}
+
+			// Otherwise, update distance and add to queue
+			dist[v.Hash()] = udist + 1
+			//q.EnqueueS(v, dist)
+			q.Enqueue(v)
+		}
+
+	}
+
+	return startPoint, -1
+}
 
 func main() {
 	input := bufio.NewScanner(os.Stdin)
+	strings := make([]string, 0)
 
 	for input.Scan() {
 		line := input.Text()
-		fmt.Println(line)
+		strings = append(strings, line)
 	}
+
+	maze = parseMap(strings)
+
+	fmt.Println()
+	fmt.Println(DFS())
+
 }
