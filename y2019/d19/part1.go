115a7f79cda493cb8f01b338468e31a19e726e19
Joseph Hines <jthines3@gmail.com>
2019-12-23 17:31:06 -0500

    .

--- y2019/d19/part1.go
+++ y2019/d19/part1.go
@@ -6,13 +6,61 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
+
+const (
+	STATIONARY int = 0
+	PULLED     int = 1
+)
+
+type Point struct {
+	x, y int
+}
+
+func countBeamLocations(ic *IntCode, size int) int {
+	points := make([]Point, 0)
+	for i := 0; i < size; i++ {
+		for j := 0; j < size; j++ {
+			points = append(points, Point{j, i})
+		}
+	}
+
+	count := 0
+	viewMap := make(map[Point]string, 0)
+
+	for _, point := range points {
+		cp := ic.Copy()
+
+		cp.AddInput(point.x)
+		cp.AddInput(point.y)
+
+		status := cp.Run()
+		if int(status) == PULLED {
+			count++
+			viewMap[point] = "#"
+		} else {
+			viewMap[point] = "."
+		}
+	}
+
+	for i := 0; i < size; i++ {
+		for j := 0; j < size; j++ {
+			fmt.Print(viewMap[Point{j, i}])
+		}
+		fmt.Println()
+	}
+
+	return count
+}
 
 func main() {
 	input := bufio.NewScanner(os.Stdin)
+	input.Scan()
+	line := input.Text()
+	tokens := strings.Split(line, ",")
 
-	for input.Scan() {
-		line := input.Text()
-		fmt.Println(line)
-	}
+	ic := NewIntCode(tokens)
+
+	fmt.Println(countBeamLocations(ic, 50))
 }
--- y2019/d19/part2.go
+++ y2019/d19/part2.go
@@ -1,4 +1,4 @@
-// Part 2
+// Part 1
 
 package main
 
@@ -6,13 +6,63 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
+
+const (
+	STATIONARY int = 0
+	PULLED     int = 1
+)
+
+type Point struct {
+	x, y int
+}
+
+func getClosestSquare(ic *IntCode, size int) int {
+
+	x := 0
+	y := 10
+	for {
+		fmt.Println(x, y)
+		cp := ic.Copy()
+
+		cp.AddInput(x)
+		cp.AddInput(y)
+
+		status := cp.Run()
+
+		// Point is in beam
+		if int(status) == PULLED {
+
+			cp := ic.Copy()
+
+			cp.AddInput(x + (size - 1))
+			cp.AddInput(y - (size - 1))
+
+			status := cp.Run()
+
+			// Opposite point is in beam
+			if int(status) == PULLED {
+				return (x * 10000) + (y - (size - 1))
+			}
+
+			x = 0
+			y++
+			continue
+		}
+		x++
+	}
+
+	return 0
+}
 
 func main() {
 	input := bufio.NewScanner(os.Stdin)
+	input.Scan()
+	line := input.Text()
+	tokens := strings.Split(line, ",")
 
-	for input.Scan() {
-		line := input.Text()
-		fmt.Println(line)
-	}
+	ic := NewIntCode(tokens)
+
+	fmt.Println(getClosestSquare(ic, 100))
 }
