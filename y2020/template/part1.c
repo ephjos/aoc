681f2e671c5162b7178059303d4da1796b8ea5c2
Joseph Hines <jthines3@gmail.com>
2020-12-08 08:39:52 -0500

    Updated template, day 8 part 1 done

--- y2020/template/part1.c
+++ y2020/template/part1.c
@@ -2,46 +2,17 @@
 #include <stdio.h>
 #include <string.h>
 
-void ehandler(char* s, int exit_code)
-{
-	fprintf(stderr, "%s\n", s);
-	exit(exit_code);
-}
-
-long num_lines(FILE* fp)
-{
-	long count = 0;
-	char buf[128] = {0};
-	fseek(fp, 0, 0);
-	for (; fscanf(fp,"%s\n", buf) == 1; ) {
-		count++;
-  }
-
-	fseek(fp, 0, 0);
-	return count;
-}
+#include "../include/cge.h"
 
 int main(int argc, char *argv[])
 {
-	FILE* fp = fopen("./input", "r");
-	if (fp == NULL) {
-		ehandler("Could not load ./input file", 1);
+	int n = 0;
+	char** lines = load_file("./input", 0, &n);
+
+	for (int i = 0; i < n; i++) {
+		printf("%s\n", lines[i]);
 	}
 
-	long n = num_lines(fp);
-
-	// Read data
-	long* vs = (long*)calloc(1, n*sizeof(long));
-	char buf[20] = {0};
-	for (int i = 0; fscanf(fp,"%s\n", buf) == 1; i++) {
-		vs[i] = atol(buf);
-  }
-
-	// Call function on data here
-
-	// Cleanup
-	free(vs);
-	fclose(fp);
-
+	ffree((void**)lines, n);
 	return 0;
 }
