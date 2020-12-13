bcecb6752502414fda8cdc4606ada5e347ab1845
Joseph Hines <jthines3@gmail.com>
2020-12-13 16:51:13 -0500

    Updated template

--- y2020/template/part1.c
+++ y2020/template/part1.c
@@ -7,10 +7,14 @@
 int main(int argc, char *argv[])
 {
 	int n = 0;
-	char** lines = load_file("./input", 0, &n);
+	char** lines;
 
-	for (int i = 0; i < n; i++) {
-		printf("%s\n", lines[i]);
+	if (argc == 3 &&
+			(strcmp(argv[1], "-f") == 0 ||
+			 strcmp(argv[1], "--file") == 0)) {
+		lines = load_file(argv[2], 0, &n);
+	} else {
+		lines = load_file("./input", 0, &n);
 	}
 
 	ffree((void**)lines, n);
