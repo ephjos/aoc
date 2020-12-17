42b4cdea41809ad8ff89523df3613d634e2b6efb
Joseph Hines <jthines3@gmail.com>
2020-12-17 17:43:19 -0500

    Day 17 Part 1

--- y2020/d16/part2.c
+++ y2020/d16/part2.c
@@ -4,6 +4,39 @@
 
 #include "cge.h"
 
+typedef struct __range range;
+typedef struct __range {
+	int l, h;
+} range;
+
+range* init_range(short l, short h)
+{
+	range* r = malloc(sizeof(range));
+	r->l = l; r->h = h;
+	return r;
+}
+
+int rangecmp(const void* a, const void* b)
+{
+	range* ra = (range*)a;
+	range* rb = (range*)b;
+
+	//printf("(%d, %d) < (%d, %d)\n", ra->l, ra->h, rb->l, rb->h);
+	return ra->l > rb->l || (ra->l == rb->l && ra->h > rb->h);
+}
+
+#define ON_RANGE( r, x ) ((r.l <= x) && (x <= r.h))
+
+int on_ranges(range* ranges, int n, int x)
+{
+	int res = 0;
+	for (int i = 0; i < n; i++) {
+		//printf("(%d, %d)\n", ranges[i].l, ranges[i].h);
+		res |= ON_RANGE(ranges[i], x);
+	}
+	return res;
+}
+
 int main(int argc, char *argv[])
 {
 	int n = 0;
@@ -17,6 +50,76 @@ int main(int argc, char *argv[])
 		lines = load_file("./input", 0, &n);
 	}
 
+	char** toks; int m;
+	long* nums;
+	int i = 0, n_rules = 0, n_ranges = 0;
+	int l1, h1, l2, h2;
+
+	// Count rules
+	while (i < n && strcmp(lines[i], "") != 0) {
+		i++; n_rules++;
+	}
+	i = 0;
+	n_ranges = n_rules*2;
+
+	// Read rules
+	range* ranges = calloc(1, sizeof(range)*n_ranges);
+	while (i < n && strcmp(lines[i], "") != 0) {
+		toks = split(lines[i], ":", &m);
+		sscanf(toks[1], " %d-%d or %d-%d", &l1, &h1, &l2, &h2);
+		FFREE(toks, m);
+		ranges[i*2] = (range){ .l = l1, .h = h1 };
+		ranges[(i*2)+1] = (range){ .l = l2, .h = h2 };
+		i++;
+	}
+	i++;
+	i++;
+
+	// Read my ticket
+	int myticket;
+	while (i < n && strcmp(lines[i], "") != 0) {
+		// NOP
+		myticket = i;
+		i++;
+	}
+	i++;
+	i++;
+
+	int ret = 0, sum = 0, valid = 1, k = 0, ioff = i;
+	int* valid_inds = malloc(sizeof(int)*(n-ioff));
+	long** valid_nums = calloc(1, sizeof(long*)*(n-ioff));
+	// Read nearby tickets
+	while (i < n && strcmp(lines[i], "") != 0) {
+		toks = split(lines[i], ",", &m);
+		nums = sstol(toks, m);
+		valid = 1;
+		for (int j = 0; j < m; j++) {
+			if (!on_ranges(ranges, n_ranges, nums[j])) {
+				sum += nums[j];
+				valid = 0;
+			}
+		}
+
+		if (valid) {
+			valid_nums[k] = nums;
+		} else {
+			free(nums);
+		}
+
+		FFREE(toks, m);
+		i++; k++;
+	}
 	FFREE(lines, n);
+
+	for (int i = 0; i < k; i++) {
+		for (int j = 0; j < m; j++) {
+			printf("%ld ", valid_nums[i][j]);
+		}
+		printf("\n");
+	}
+
+	DUMP("%d", sum);
+
+	free(ranges);
 	return 0;
 }
---
+++ y2020/d17/part1.c
@@ -0,0 +1,134 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "cge.h"
+
+const int DEAD = 0;
+const int ALIVE = 1;
+
+const char DEADC = '.';
+const char ALIVEC = '#';
+
+int*** new_space(int N)
+{
+	int*** space = malloc(sizeof(int**)*N);
+	for (int i = 0; i < N; i++) {
+		space[i] = malloc(sizeof(int*)*N);
+		for (int j = 0; j < N; j++) {
+			space[i][j] = malloc(sizeof(int)*N);
+			for (int k = 0; k < N; k++) {
+				space[i][j][k] = DEAD;
+			}
+		}
+	}
+	return space;
+}
+
+void free_space(int*** space, int N)
+{
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			free(space[i][j]);
+		}
+		free(space[i]);
+	}
+	free(space);
+}
+
+int main(int argc, char *argv[])
+{
+	int n = 0;
+	char** lines;
+
+	if (argc == 3 &&
+			(strcmp(argv[1], "-f") == 0 ||
+			 strcmp(argv[1], "--file") == 0)) {
+		lines = load_file(argv[2], 0, &n);
+	} else {
+		lines = load_file("./input", 0, &n);
+	}
+
+	int o = 24; // padding around input slice
+	int N = n+o; // add padding
+	int c = N / 2; // center
+
+	// Initiaize space
+	int*** space = new_space(N);
+
+	// Initiaize slice
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			// Center in center slice
+			space[c][i+(o/2)][j+(o/2)] = lines[i][j] == ALIVEC;
+		}
+	}
+
+	// Print slice
+	/*
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			printf("%d ", space[c][i][j]);
+		}
+		if (i == N-1) printf("\n");
+	}
+	*/
+
+
+	// Run steps
+	int alive_neighbors;
+	int*** n_space, ***p_space = space;
+	for (int s = 0; s < 6; s++) {
+		n_space = new_space(N);
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				for (int k = 0; k < N; k++) {
+					alive_neighbors = 0;
+					for (int x = -1; x <= 1; x++) {
+						for (int y = -1; y <= 1; y++) {
+							for (int z = -1; z <= 1; z++) {
+								if (x == y && y == z && z == 0) continue; // skip self
+								if (IS_BETWEEN(i+x, 0, N-1) && IS_BETWEEN(j+y, 0, N-1) &&
+										IS_BETWEEN(k+z, 0, N-1)) {
+									alive_neighbors += p_space[i+x][j+y][k+z];
+								}
+							}
+						}
+					}
+					n_space[i][j][k] = ((p_space[i][j][k] && (alive_neighbors == 2 || alive_neighbors == 3))
+								|| (!p_space[i][j][k] && alive_neighbors == 3));
+				}
+			}
+		}
+
+		// Print slice
+		/*
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				printf("%d ", n_space[c][i][j]);
+			}
+			printf("\n");
+			if (i == N-1) printf("\n");
+		}
+		*/
+		free_space(p_space, N);
+		p_space = n_space;
+	}
+
+	// Count alive
+	int sum = 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			for (int k = 0; k < N; k++) {
+				sum += n_space[i][j][k];
+			}
+		}
+	}
+
+	DUMP("%d", sum);
+
+	// Cleanup
+	free_space(n_space, N);
+	FFREE(lines, n);
+	return 0;
+}
