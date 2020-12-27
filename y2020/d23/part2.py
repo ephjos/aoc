09efa2f77fa5de5585c36f631ad2456825e9423a
Joseph Hines <jthines3@gmail.com>
2020-12-27 14:17:30 -0500

    Day 23 part 2 done

--- y2020/d23/part2.py
+++ y2020/d23/part2.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 import argparse
+from tqdm import tqdm
 
 class CupGame:
-    def __init__(self, line):
-        self.input = line.strip()
-        self.cups = [0 for i in range(10)]
+    def __init__(self, line, max_val=1000000):
+        self.input = [int(x) for x in line.strip()]
+        self.max = max_val
+        self.cups = [0 for i in range(self.max+1)]
+        start = self.input[0]
         for i, c in enumerate(self.input):
-            self.cups[int(c)] = int(self.input[(i+1)%9])
-        self.current = int(self.input[0])
+            self.cups[c] = self.input[(i+1)%len(self.input)]
+            prev = c
+        for i in range(len(self.input)+1, self.max+1):
+            self.cups[prev] = i
+            prev = i
+        self.cups[prev] = start
+        self.current = start
 
     def step(self):
         c1 = self.cups[self.current]
@@ -16,10 +24,10 @@ class CupGame:
         after = self.cups[c3]
 
         dest = self.current-1
-        if dest == 0: dest = 9
+        if dest == 0: dest = self.max
         while dest == c1 or dest == c2 or dest == c3:
             dest -= 1
-            if dest == 0: dest = 9
+            if dest == 0: dest = self.max
 
         self.cups[self.current] = after  # 'remove' next 3
 
@@ -28,24 +36,18 @@ class CupGame:
         self.cups[c3] = after_dest       # complete insert after dest
         self.current = after             # set current for next iteration
 
-    def simulate(self, n=100):
-        for i in range(n):
+    def simulate(self, n=10000000):
+        for i in tqdm(range(n)):
             self.step()
-        return str(self)
-
-    def __str__(self):
-        out = ''
-        curr = self.cups[1]
-        while curr != 1:
-            out += str(curr)
-            curr = self.cups[curr]
-        return out
+        a = self.cups[1]
+        b = self.cups[a]
+        return a,b
 
 def main(fn):
     with open(fn, 'r') as fp:
         cg = CupGame(next(fp))
-        res = cg.simulate(100)
-        print(f'result = {res}')
+        res = cg.simulate()
+        print(f'result = {res}, {res[0]*res[1]}')
 
 
 if __name__ == "__main__":
