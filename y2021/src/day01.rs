742bd4b87023171277c8a071bb27f7b7406c5dc1
Joseph Hines <jthines3@gmail.com>
2021-12-01 06:09:11 -0500

    2021: day 01 correct

--- y2021/src/day01.rs
+++ y2021/src/day01.rs
@@ -1,14 +1,34 @@
 
 fn part1(input: &str) -> isize {
-    return 0;
+    let mut prev = usize::MAX;
+    let mut count = 0;
+    for line in input.lines() {
+        let curr = line.parse::<usize>().expect(&format!("Could not convert {}", line));
+        if curr > prev {
+            count += 1;
+        }
+        prev = curr;
+    }
+    return count;
 }
 
 fn part2(input: &str) -> isize {
-    return 0;
+    let lines = input.lines().map(|l| l.parse::<usize>().expect(&format!("Could not convert line {} to usize", l))).collect::<Vec<usize>>();
+    let mut prev = usize::MAX;
+    let mut count = 0;
+
+    for i in 2..lines.len() {
+        let curr = lines[i-2] + lines[i-1] + lines[i];
+        if curr > prev {
+            count += 1;
+        }
+        prev = curr;
+    }
+    return count;
 }
 
 pub fn run() {
     println!("01.1: {:?}", part1(include_str!("../input/day01")));
     println!("01.2: {:?}", part2(include_str!("../input/day01")));
 }
-  
+
