7e900b3ecbefec62201450685a40b1bb42ca329b
Joseph Hines <jthines3@gmail.com>
2021-12-02 06:02:52 -0500

    2021: day 02 complete

--- y2021/src/day02.rs
+++ y2021/src/day02.rs
@@ -1,10 +1,45 @@
 
-fn part1(input: &str) -> isize {
-    return 0;
+fn part1(input: &str) -> usize {
+    let mut x = 0;
+    let mut depth = 0;
+
+    for line in input.lines() {
+        let toks = line.split(" ").collect::<Vec<_>>();
+        let cmd = toks[0];
+        let val = toks[1].parse::<usize>().expect("Could not parse value");
+
+        match cmd {
+            "up" => depth -= val,
+            "down" => depth += val,
+            "forward" => x += val,
+            "forward" => x += val,
+            _ => (),
+        }
+    }
+    return x * depth;
 }
 
-fn part2(input: &str) -> isize {
-    return 0;
+fn part2(input: &str) -> usize {
+    let mut x = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+
+    for line in input.lines() {
+        let toks = line.split(" ").collect::<Vec<_>>();
+        let cmd = toks[0];
+        let val = toks[1].parse::<usize>().expect("Could not parse value");
+
+        match cmd {
+            "up" => aim -= val,
+            "down" => aim += val,
+            "forward" => {
+                x += val;
+                depth += aim * val;
+            },
+            _ => (),
+        }
+    }
+    return x * depth;
 }
 
 pub fn run() {
