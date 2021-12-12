0a2bfd680494492cd8571e317549e06a827aa997
Joseph Hines <jthines3@gmail.com>
2021-12-12 13:01:42 -0500

    2021: Day 12 Complete

--- y2021/src/main.rs
+++ y2021/src/main.rs
@@ -1,28 +1,34 @@
-//mod day01;
-//mod day02;
-//mod day03;
-//mod day04;
-//mod day05;
-//mod day06;
-//mod day07;
-//mod day08;
-//mod day09;
-//mod day10;
+/*
+mod day01;
+mod day02;
+mod day03;
+mod day04;
+mod day05;
+mod day06;
+mod day07;
+mod day08;
+mod day09;
+mod day10;
 mod day11;
+*/
+mod day12;
 
 pub mod com;
 
 fn main() {
     println!("Advent of Code 2021");
-    //day01::run();
-    //day02::run();
-    //day03::run();
-    //day04::run();
-    //day05::run();
-    //day06::run();
-    //day07::run();
-    //day08::run();
-    //day09::run();
-    //day10::run();
+    /*
+    day01::run();
+    day02::run();
+    day03::run();
+    day04::run();
+    day05::run();
+    day06::run();
+    day07::run();
+    day08::run();
+    day09::run();
+    day10::run();
     day11::run();
+    */
+    day12::run();
 }
