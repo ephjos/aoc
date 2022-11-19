d7f78d7c757f2c5e50f849539b611ceaec63b16e
Joseph Hines <jthines3@gmail.com>
2022-11-19 11:23:39 -0500

    Finish y2015 d01, sort out initial template + structure

---
+++ y2015/src/main.zig
@@ -0,0 +1,5 @@
+const d01 = @import("./d01.zig");
+
+pub fn main() void {
+    d01.main();
+}
---
+++ y2015/src/utils.zig
@@ -0,0 +1,6 @@
+const std = @import("std");
+
+pub const Case = struct {
+    input: []const u8,
+    expected: isize,
+};
