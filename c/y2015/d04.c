c84aec905128082cc9a25ac23b35ca2f146be1c9
Joseph Hines <ephjosdev@gmail.com>
2023-05-13 14:28:55 -0400

    c.2015.4 improvements

--- c/y2015/d04.c
+++ c/y2015/d04.c
@@ -1,6 +1,17 @@
+#include <stdio.h>
 #define YEAR 2015
 #define DAY 4
 #include "../shared_2015.h"
+  
+unsigned char digest[16];
+
+// https://stackoverflow.com/a/8389763
+static inline void md5_leading_zeros(const char *s, int32_t length, unsigned char *out) {
+  MD5_CTX c;
+  MD5_Init(&c);
+  MD5_Update(&c, s, length);
+  MD5_Final(out, &c);
+}
 
 int32_t main(int32_t argc, char **argv) {
   struct input_file file = get_input_file();
@@ -8,44 +19,31 @@ int32_t main(int32_t argc, char **argv) {
   file.bytes[file.len-1] = '\0';
 
   int32_t i = 0;
-  int32_t suffix_1 = -1;
-  int32_t suffix_2 = -1;
-  char buf[64] = {0};
+  int32_t suffix_1 = 0;
+  int32_t suffix_2 = 0;
+  uint64_t l = file.len+8;
+  char *buf = malloc(l);
+  int prefix_len = snprintf(buf, l, "%s", file.bytes);
+  uint64_t rl = l-prefix_len;
 
-  for (i = 0; i < INT_MAX; i++) {
-    snprintf(buf, 64, "%s%d", file.bytes, i);
-    char *hash = str2md5(buf, (int32_t)strlen(buf));
+  while (!suffix_1 || !suffix_2) {
+    int x = snprintf(buf+prefix_len, rl, "%d", i);
 
-    if (
-        suffix_1 == -1 &&
-        hash[0] == '0' &&
-        hash[1] == '0' &&
-        hash[2] == '0' &&
-        hash[3] == '0' &&
-        hash[4] == '0'
-        ) {
-      suffix_1 = i;
-      part_1("%d", suffix_1);
+    md5_leading_zeros(buf, prefix_len+x, digest);
+
+    int num_zeros = (4 * (!digest[0] && !digest[1])) + (digest[2] < 0x10) + (digest[2] == 0);
+    if (!suffix_1 && num_zeros >= 5) {
+        suffix_1 = i;
+        part_1("%d", suffix_1);
+    }
+    if (!suffix_2 && num_zeros >= 6) {
+        suffix_2 = i;
+        part_2("%d", suffix_2);
     }
 
-    if (
-        suffix_2 == -1 &&
-        hash[0] == '0' &&
-        hash[1] == '0' &&
-        hash[2] == '0' &&
-        hash[3] == '0' &&
-        hash[4] == '0' &&
-        hash[5] == '0'
-        ) {
-      suffix_2 = i;
-      part_2("%d", suffix_2);
-    }
-
-    if (suffix_1 != -1 && suffix_2 != -1) {
-      break;
-    }
+    i++;
   }
-  
+
   if (suffix_1 == -1) {
     eprintf("Could not find answer for part 1, tried %d numbers\n", i);
   }
@@ -54,6 +52,7 @@ int32_t main(int32_t argc, char **argv) {
     eprintf("Could not find answer for part 2, tried %d numbers\n", i);
   }
 
+  free(buf);
   free_input_file(&file);
   return 0;
 }
